a8d1e539ef1d32e9d5e0a367db27cc16282c0b4f
Francisco Diaz <diazsalerno2@outlook.es>
2023-05-06 15:00:07 +0200

    create auth and user services and store.js

---
+++ src/_services/userService.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { global } from "../_global/global";
+
+const userService = {};
+
+// userService.getStudents = async (token) => {
+//   const config = {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//   };
+
+//   return (await axios.get(global.BASE_URL + "/api/alumnos?page=1", config))
+//     .data;
+// };
+
+userService.getAllPatients = async (token, page = 1) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  return (await axios.get(global.BASE_URL + `/users/profile/checkallpatients?page=${page}`, config))
+    .data;
+};
+
+export default userService;
