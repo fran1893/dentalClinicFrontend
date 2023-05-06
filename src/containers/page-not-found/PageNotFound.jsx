d2c7265bd9f319606442adcabc71eadd398f91cb
Francisco Diaz <diazsalerno2@outlook.es>
2023-05-06 15:05:18 +0200

    Create TablePagination container

--- src/containers/page-not-found/PageNotFound.jsx
+++ src/containers/page-not-found/PageNotFound.jsx
@@ -2,5 +2,10 @@ import React from "react";
 import "./PageNotFound.scss";
 
 export default function PageNotFound() {
-  return <h1>PageNotFound</h1>;
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <h2>Error 404</h2>
+    </div>
+  );
 }
